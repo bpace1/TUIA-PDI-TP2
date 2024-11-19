ba74e9d6e63f31aff6a48dbc6f89e1b79d6fd312
Bruno Pace <82056912+bpace1@users.noreply.github.com>
2024-11-19 11:58:31 -0300

    Update TP2_ej1.py
    detection advances

--- TP2_ej1.py
+++ TP2_ej1.py
@@ -8,15 +8,15 @@ DATA_PATH = os.path.join(PATH, 'data')
 
 monedas: np.ndarray =  cv2.imread(os.path.join(DATA_PATH,'monedas.jpg'))
 monedas_gray: np.ndarray = cv2.cvtColor(monedas, cv2.COLOR_BGR2GRAY)
-
-plt.imshow(monedas, cmap='gray')
+plt.imshow(monedas_gray, cmap='gray')
 plt.show()
 
-monedas_blur: np.ndarray = cv2.GaussianBlur(monedas_gray, (5, 5), 0)
+monedas_blur: np.ndarray = cv2.GaussianBlur(monedas_gray, (3, 3), 0)
+
 
 detected_circles = cv2.HoughCircles(monedas_blur,  
-                   cv2.HOUGH_GRADIENT, 1.2, 100, param1 = 100, 
-               param2 = 150, minRadius = 70, maxRadius = 200)     
+                   cv2.HOUGH_GRADIENT, 1.2, 90, param1 = 65, 
+               param2 = 170, minRadius = 70, maxRadius = 200)     
 
 print(f'Circles detected: {detected_circles}')
 
@@ -30,6 +30,8 @@ if detected_circles is not None:
     for pt in detected_circles[0, :]:
         a, b, r = pt[0], pt[1], pt[2]
         
+        cv2.circle(monedas_with_circles, (a, b), r, (0, 0, 0), -1)  # Interior negro
+
         # Draw the circumference of the circle.
         cv2.circle(monedas_with_circles, (a, b), r, (0, 255, 0), 2)
         
@@ -41,7 +43,39 @@ if detected_circles is not None:
 monedas_with_circles_rgb = cv2.cvtColor(monedas_with_circles, cv2.COLOR_BGR2RGB)
 
 # Display the result using Matplotlib
-plt.imshow(monedas_with_circles_rgb)
+plt.imshow(monedas_with_circles_rgb, cmap='gray')
 plt.axis("off")  # Hide the axes
 plt.title("Detected Circles")
 plt.show()
+
+# Aplicar un umbral para binarizar la imagen
+_, monedas_negras = cv2.threshold(monedas_with_circles_rgb, 1, 255, cv2.THRESH_BINARY)
+
+print(monedas_negras)
+
+# Mostrar la imagen binarizada
+plt.imshow(monedas_negras, cmap='gray')
+plt.axis('off')  # Desactivar los ejes
+plt.title("Binarized Image (0 and 1)")
+plt.show()
+
+
+mask = monedas_negras == 255
+monedas_selected = monedas_gray.copy()
+monedas_selected[mask] = 0  # Establecer los valores de monedas_selected a 0 donde la máscara es True
+
+# Mostrar la imagen final con solo los píxeles seleccionados de monedas_gray
+plt.imshow(monedas_selected, cmap='gray')
+plt.axis('off')  # Desactivar los ejes
+plt.title("Filtered Image from Monedas Gray")
+plt.show()
+
+# Ver la matriz de monedas_selected (opcional)
+print(np.unique(monedas_selected))
+
+
+
+
+
+
+
